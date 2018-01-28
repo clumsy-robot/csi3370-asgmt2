df7dc4e03f507631e6e1c41d76ae2cbac1f61dab
Kelly Kurkowski <kkurkowski@oakland.edu>
2018-01-27 21:47:57 -0500

    Created initial project files.

---
+++ src/assignment02/Assignment02.java
@@ -0,0 +1,18 @@
+package assignment02;
+
+/**
+ *
+ * @author Kelly Kurkowski
+ * CSI 3370 - Assignment #2
+ * Due Monday, January 29, 2018
+ */
+public class Assignment02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
