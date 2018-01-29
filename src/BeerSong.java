2af995442f5b64e26e625a92e5fe65ca93a326a1
Kelly Kurkowski <kkurkowski@oakland.edu>
2018-01-28 21:02:35 -0500

    Implemented Ninety_Nine_Bottles_of_Beer() method in BeerSong.java

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -12,6 +12,16 @@ public class BeerSong {
     }
     
     public void Nintey_Nine_Bottles_of_Beer() {
+        int bottles = 99;
         
+        while (bottles != 1) {
+            System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer");
+            bottles--;
+            System.out.println("take one down, pass it around, " + bottles + " bottles of beer on the wall.");
+        }
+        
+        System.out.println(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer");
+        bottles--;
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
     }
 }
